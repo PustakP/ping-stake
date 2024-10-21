6c594f806c87c6c332ddc09b46d2a2c299093c7b
Lalit M <lalitm1004@gmail.com>
2024-10-21 11:25:03 +0530

    make typing

---
+++ utils/types.py
@@ -0,0 +1,18 @@
+from typing import List, NamedTuple, Tuple
+
+class Gamble(NamedTuple):
+    amount_bet: float
+    win_percentage: float
+
+MutableHistory = List[bool]
+History = Tuple[bool, ...]
+
+class Strategy:
+    def __init__(self):
+        self.name: str = ''
+    
+    def begin(self, balance: float, rounds_left: int) -> Gamble:
+        return
+
+    def turn(self, balance: float, rounds_left: int, history: History) -> Gamble:
+        return
